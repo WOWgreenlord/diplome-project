ac5b32776922654a9ed4b1b43728873a2c0ab73e
Миша Дьяков <dock.misha@gmail.com>
2024-10-20 13:46:32 +0300

    добавил header и первую верстку

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { TreolanService } from './services/treolan.service';
 import { Token } from './interfaces/Token';
 import { SidebarComponent } from "./sidebar/sidebar.component";
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SidebarComponent],
+  imports: [RouterOutlet, SidebarComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,23 @@
+import { Component, inject, OnInit } from '@angular/core';
+
+import { Token } from '../interfaces/Token';
+import { TreolanService } from '../services/treolan.service';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent implements OnInit {
+  treolan = inject(TreolanService)
+  token: Token | null = null;
+  
+  ngOnInit(): void {
+    this.treolan.token$.subscribe((token) => {
+      this.token = token;
+    })
+  }
+
+}
