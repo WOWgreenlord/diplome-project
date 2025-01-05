093e219f736aa92baaddc374a55ebaff22d9ab35
se4en <001122mj@gmail.com>
2025-01-05 22:03:37 +0300

    подробная информация о товаре

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 
 import { MouserService } from '../../services/mouser.service';
 import { Product } from '../../interfaces/Product';
+import { FavoritesService } from '../../services/favorites.service';
 
 @Component({
   selector: 'app-product',
@@ -14,16 +15,34 @@ import { Product } from '../../interfaces/Product';
 export class ProductComponent implements OnInit {
   mouser = inject(MouserService);
   router = inject(ActivatedRoute);
+  favorite = inject(FavoritesService);
   mouserPartNumber: string = '';
   product?: Product;
+  price: number[] = [];
 
   ngOnInit(): void {
     this.router.paramMap.subscribe((params) => {
       this.mouserPartNumber = params.get('MouserPartNumber') || '';
     })
     this.mouser.productData$.subscribe((data) => {
-      console.log(data);
-      this.product = data;
-    })
+      if (data) {
+        this.product = data.find((item: Product) => item.MouserPartNumber === this.mouserPartNumber);
+      }
+
+      // Если данные не найдены, можно отправить запрос к серверу
+      if (!this.product) {
+        this.loadProductDetails();
+      }
+    });
+  }
+  // Метод для загрузки деталей товара с сервера (по необходимости)
+  loadProductDetails(): void {
+    this.mouser.postProducts(this.mouserPartNumber).subscribe((response) => {
+      this.product = response.SearchResults.Parts[0]; // Пример: настройте в зависимости от структуры данных
+      console.log(this.price);
+    });
+  }
+  addToFavorites(product: Product) {
+    this.favorite.addToFavorites(product);
   }
 }
